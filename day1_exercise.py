262b8d470ce95de657e46ac18a94c83927fd6e12
Aashiq <adams.aashiq@gmail.com>
2020-09-21 13:45:54 +0200

    Python exercise day 1

---
+++ day1_exercise.py
@@ -0,0 +1,14 @@
+name = input("Enter your name: ")
+
+print("Hello "+name+"! How are you?")
+
+age = int(input("Enter your age: "))
+
+print("So in 10 years you will be...")
+print(str(age+10)+" years old!")
+
+rad = float(input("Enter the radius: "))
+
+circum = 2*3.1415*rad
+
+print("The circumference is "+str(round(circum, 3)))
