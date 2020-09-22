c8a27741a47d7e1e48940accba57159783a01002
Aashiq <adams.aashiq@gmail.com>
2020-09-22 15:18:09 +0200

    Python day 2 exercise

---
+++ day2_exercise.py
@@ -0,0 +1,20 @@
+x = input("Type some words: \n")
+print("First Letter: "+x[0])
+print("Last letter: "+x[-1])
+
+y = input("Would you like to convert these words to piglatin? (y/n): ")
+
+if y == 'y':
+    x = x.split()
+    for i in range(len(x)):
+        if x[i][0] in "aeiou":
+             x[i] += 'yay'
+        else:
+             x[i]=x[i][1:]+x[i][0]
+             x[i]+='ay'
+    x = ' '.join(x)
+    print(x)
+elif y == 'n':
+    print("Understandable, have a nice day!")
+else:
+    print("You have to enter either 'y' or 'n'. Program will now end")
