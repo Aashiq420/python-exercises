c4aaa605007b03a6982aab9bb064d8fba9d49c22
Aashiq <adams.aashiq@gmail.com>
2020-09-25 09:49:40 +0200

    added mark grader

--- friday.py
+++ friday.py
@@ -8,6 +8,8 @@ elif n2>n1:
 else:
     print("The numbers are equal")'''
 
+'''*************************************************************'''
+
 '''x=10
 if x<=15:
     if x==10:
@@ -17,15 +19,26 @@ if x<=15:
 else:
     print("dont play a game")'''
 
+'''***************************************************************'''
+
 mark = float(input("Enter the student's mark as percentage: "))
  
-if mark>=80 and mark<=100:
-     print("Grade A")
-elif mark>=70 and mark<=79:
-    print("Grade B")
-elif mark>=50 and mark<=69:
-    print("Grade C")
-elif mark>=40 and mark<=49:
-    print("Grade D")
+while mark <=100 and mark>=0:
+    if mark>=80 and mark<=100:
+        print("Grade A")
+        break
+    elif mark>=70 and mark<=79:
+        print("Grade B")
+        break
+    elif mark>=50 and mark<=69:
+        print("Grade C")
+        break
+    elif mark>=40 and mark<=49:
+        print("Grade D")
+        break
+    else:
+        print("Grade F")
+        break
 else:
-    print("Grade F")
+    print("It is not possible to have more than 100%, or less than 0%")
+    
