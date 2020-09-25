228613acc7b5608eb2ef7ec3435d7c84aea2d0aa
Aashiq <adams.aashiq@gmail.com>
2020-09-25 09:10:08 +0200

    Friday python number sorter

---
+++ friday.py
@@ -0,0 +1,9 @@
+n1 = float(input("Enter a number: "))
+n2 = float(input("Enter another number: "))
+
+if n1>n2:
+    print(str(n1)+" is greater.")
+elif n2>n1:
+    print(str(n2)+" is greater.")
+else:
+    print("The numbers are equal")
