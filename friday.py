4f367a82deed300632b3792142922d3a7f4b6604
Aashiq <adams.aashiq@gmail.com>
2020-09-25 09:37:08 +0200

    added mark grader

--- friday.py
+++ friday.py
@@ -1,4 +1,4 @@
-n1 = float(input("Enter a number: "))
+'''n1 = float(input("Enter a number: "))
 n2 = float(input("Enter another number: "))
 
 if n1>n2:
@@ -6,4 +6,26 @@ if n1>n2:
 elif n2>n1:
     print(str(n2)+" is greater.")
 else:
-    print("The numbers are equal")
+    print("The numbers are equal")'''
+
+'''x=10
+if x<=15:
+    if x==10:
+        print("play cricket")
+    else:
+        print("play kabadi")    
+else:
+    print("dont play a game")'''
+
+mark = float(input("Enter the student's mark as percentage: "))
+ 
+if mark>=80 and mark<=100:
+     print("Grade A")
+elif mark>=70 and mark<=79:
+    print("Grade B")
+elif mark>=50 and mark<=69:
+    print("Grade C")
+elif mark>=40 and mark<=49:
+    print("Grade D")
+else:
+    print("Grade F")
