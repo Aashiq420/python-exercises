ebff936cd8f6ceae02e4982debdbf41982597058
Aashiq <adams.aashiq@gmail.com>
2020-09-25 09:04:18 +0200

    Number sorter from list

---
+++ operators2.py
@@ -0,0 +1,6 @@
+'''Task 2 number sorter'''
+my_numbers = [2,56,12,67,1000,32,89,29,44,39,200,11,21]
+new_num = sorted(my_numbers)
+
+print("The smallest number is: "+str(new_num[0]))
+print("The largest number is: "+str(new_num[-1]))
