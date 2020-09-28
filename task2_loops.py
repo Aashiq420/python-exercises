5197ac33fcb7a9b2fc7d3a11a6d8f028ca5de51d
Aashiq <adams.aashiq@gmail.com>
2020-09-28 13:18:02 +0200

    Minor changes

--- task2_loops.py
+++ task2_loops.py
@@ -1,9 +1,8 @@
 for i in range(1,6):
     x="*"
     print(x*i)
-    i=i+1
+    i+=1
     if i==6:
         for j in reversed(range(1,5)):
             y="*"
             print(y*j)
-            j=j+1
