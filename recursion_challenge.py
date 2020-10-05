5bd7055d842ececb93af130e59f5baed62875c58
Aashiq <adams.aashiq@gmail.com>
2020-10-05 10:52:54 +0200

    minor changes

--- recursion_challenge.py
+++ recursion_challenge.py
@@ -1,3 +1,4 @@
+'''Creating a function to check if input is a palindrome'''
 def palindrome_check(x):
     x = list(str(x))
     if x[0:] == list(reversed(x[0:])):
@@ -6,13 +7,16 @@ def palindrome_check(x):
         return False
 
 
-print("*******************")
+print("\n*******************")
 print("PALINDROME CHECKER")
 print("*******************\n")
 
+'''Asking for user to input something to check 
+and running it through the palindrome check function'''
 n=input("Enter a word or number to check: ")
 ans=palindrome_check(n)
 
+'''analyzing output of function and returning a message'''
 if ans==True:
     print(n,"is a palindrome")
 else:
