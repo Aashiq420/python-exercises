c396559642e0b88b9c956278639249e21a347679
Aashiq <adams.aashiq@gmail.com>
2020-11-06 15:53:09 +0200

    machine learning task

---
+++ machine_learning.py
@@ -0,0 +1,35 @@
+#Aashiq Adams
+#importing necessary modules
+import numpy as np
+import matplotlib.pyplot as plt
+
+#CHALLENGE 1
+#creates an array of numbers in the given range
+nums = np.arange(start=1, stop=21, step=1)
+
+#using numpy functions to determine mean, std deviation, and variance
+#of the above generated array of numbers
+#as well as displaying them with an appropriate message
+mean_nums = np.mean(nums)
+print("Mean:",mean_nums)
+
+std_dev = np.std(nums)
+print("Standard Deviation:",round(std_dev,3))
+
+variance = np.var(nums)
+print("Variance",variance)
+
+#CHALLENGE 2
+#creates lists of data
+nums_1 = [0.5, 0.7, 1.0, 1.2, 1.3, 2.1]
+bins_1 = [0, 1, 2, 3]
+
+#Naming labels and giving title
+plt.title("Histogram showing nums vs bins")
+plt.xlabel("Nums")
+plt.ylabel("Bins")
+
+#plotting the histogram with above information
+plt.hist(nums_1, bins=bins_1, edgecolor='black')
+
+plt.show()
